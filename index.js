ec3cf888e1d67c6579a2acf9334fa88a5e4fc3bc
MuhammadOAsif <muhammadasif2m@gmail.com>
2022-08-03 15:41:23 +0100

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
 
